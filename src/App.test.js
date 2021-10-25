4eb1d9f565e68bd681011620f3e371a8c003777f
jonkul <jonkullberg@gmail.com>
2021-10-25 14:52:32 +0200

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,114 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
 import App from './App';
+import {
+  fireEvent,
+  render,
+  screen,
+  act
+} from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+
+describe('App', () => {
+    test('clicks New button', async () => {
+        render(<App />);
+
+        //sets up for the final test
+        var mhm = await screen.findByText('New');
+        act(() => {
+            userEvent.click(mhm);
+        });
+    });
+
+
+
+    test('renders App component, finds expected static elements', async () => {
+        render(<App />);
+
+        expect(screen.getByText('My little React/Trix text editor')).toBeInTheDocument();
+        expect(screen.getByText('Active document:')).toBeInTheDocument();
+        expect(screen.getByText('Documents:')).toBeInTheDocument();
+        expect(await screen.findByText('Dokument1')).toBeInTheDocument();
+    });
+
+
+
+    test('renders App component, finds expected DB elements', async () => {
+        render(<App />);
+
+        expect(await screen.findByText('Dokument1')).toBeInTheDocument();
+    });
+
+
+
+    test('edits the "text editor", finds state driven <p> in DOM', async () => {
+        render(<App />);
+
+        expect(screen.queryByText('test input2')).toBeNull();
+
+        await act(async () => {
+            fireEvent.change(screen.getByRole('textbox'), {
+                target: { value: 'test input2' },
+            });
+        });
+
+        expect(await screen.findByText('test input2')).toBeInTheDocument();
+    });
+
+
+
+    test('edits "active document input", finds state driven <p> in DOM', async () => {
+        render(<App />);
+
+        expect(screen.queryByText('test input')).toBeNull();
+
+        await act(async () => {
+            fireEvent.change(screen.getByRole('textbox'), {
+                target: { value: 'test input' },
+            });
+        });
+
+        expect(await screen.findByText('test input')).toBeInTheDocument();
+    });
+
+
+
+    test('finds "New document"', async () => {
+        render(<App />);
+
+        expect(screen.queryByText('test input3')).toBeNull();
+
+        await act(async () => {
+            fireEvent.change(screen.getByRole('textbox'), {
+                target: { value: 'test input3' },
+            });
+        });
+
+        expect(await screen.findByText('test input3')).toBeInTheDocument();
+
+        sleep(3000);
+
+        expect(await screen.findByText('New document')).toBeInTheDocument();
+        /* screen.debug(); */
+    });
+
+
+    test('clicks Reset DB button', () => {
+        render(<App />);
+
+        expect(screen.queryByText('test input3')).toBeNull();
+
+        var mhm = screen.getByText('Reset DB');
+        act(() => {
+            userEvent.click(mhm);
+        });
+
+        expect(screen.queryByText('New document')).toBeNull();
+    });
 });
